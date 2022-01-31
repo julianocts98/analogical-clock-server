43c48c56b4eb274c96c549aa09ed073620a5c9f7
Juliano Cavalcante Teixeira da Silva <juliano.cavalcante98@gmail.com>
2022-01-31 10:56:57 -0300

    Refactor code and allow timezones to be passed through rooms, including local time of owner

--- index.js
+++ index.js
@@ -72,10 +72,6 @@ function isSocketOwnerOfTimezoneRoom(socket) {
   return roomOwner[room] === socket.id;
 }
 
-function checkIfTimezoneRoomExists(timezoneRoomName) {
-  return timezoneRoomName in roomOwner;
-}
-
 async function fetchTimezones() {
   try {
     const response = await axios(WORLD_TIME_API_URL);
@@ -102,19 +98,39 @@ function isValidRoomName(roomName) {
 }
 
 function formatDatetime(datetime) {
-  return datetime.substring(0, 26);
+  return datetime.replace("Z", "").substring(0, 26);
 }
 
-function onTimezoneChanged(socket) {
-  socket.on("timezoneChanged", async (selectedTimezone) => {
-    if (isSocketInTimezoneRoom(socket) && isSocketOwnerOfTimezoneRoom(socket)) {
-      if (selectedTimezone != "local") {
-        const room = getTimezoneRoomBySocket(socket);
+async function updateDateOfARoom(ownerSocket, selectedTimezone, ownerDatetime) {
+  const room = getTimezoneRoomBySocket(ownerSocket);
+
+  function emitDatetimeOfTimezone(datetimeToEmit) {
+    io.to(room).emit("datetimeOfTimezone", datetimeToEmit, selectedTimezone);
+  }
+
+  if (selectedTimezone !== "local") {
         const datetimeOfTimezone = formatDatetime(
           await getDatetimeByTimezone(selectedTimezone)
         );
-        io.to(room).emit("datetimeOfTimezone", datetimeOfTimezone);
+    emitDatetimeOfTimezone(datetimeOfTimezone);
+  } else {
+    const datetimeOfTImezone = formatDatetime(ownerDatetime);
+    emitDatetimeOfTimezone(datetimeOfTImezone);
       }
+}
+
+function requestRefreshDateOfRoom(roomName) {
+  const owner = roomOwner[roomName];
+  io.sockets.to(owner).emit("requestDateInfo");
+}
+
+function onTimezoneChanged(socket) {
+  socket.on("timezoneChanged", async (selectedTimezone, ownerDatetime) => {
+    if (
+      isSocketInATimezoneRoom(socket) &&
+      isSocketOwnerOfTimezoneRoom(socket)
+    ) {
+      updateDateOfARoom(socket, selectedTimezone, ownerDatetime);
     } else {
       if (selectedTimezone !== "local") {
         const datetimeOfTimezone = formatDatetime(
@@ -143,65 +159,45 @@ async function onConnection(socket) {
   );
 }
 
-function onTimezoneRoomCreate(socket) {
-  socket.on("timezoneRoom:create", (timezoneRoomName) => {
-    socket.on("disconnect", () => {
-      console.log(`> User ${socket.id} disconnected!`);
-    });
-
-    if (socket.rooms.size > 1) {
-      const previousRoom = Array.from(socket.rooms)[1];
-      socket.leave(previousRoom);
-    }
-
-    if (isValidRoomName(timezoneRoomName)) {
-      if (!checkIfTimezoneRoomExists(timezoneRoomName)) {
-        roomOwner[timezoneRoomName] = socket.id;
-        socket.join(timezoneRoomName);
+async function handleTimezoneRoomJoin(socket, timezoneRoomName) {
+  function emitTimezoneRoomJoinResult(
+    message,
+    status,
+    roomName,
+    roomUsers,
+    roomOwner
+  ) {
         socket.emit(
-          "timezoneRoom:create:result",
-          `Room created`,
-          true,
-          timezoneRoomName,
-          roomOwner[timezoneRoomName]
-        );
-      } else {
-        socket.emit(
-          "timezoneRoom:create:result",
-          `Room name already taken! Try another`,
-          false
+      "timezoneRoom:join:result",
+      message,
+      status,
+      roomName,
+      roomUsers,
+      roomOwner
         );
       }
-    } else {
-      socket.emit(
-        "timezoneRoom:create:result",
-        "Invalid room name!\n Length must be higher than just one character.\n\" '  ` and whitespaces are not allowed either!",
-        false
-      );
+
+  if (!timezoneRoomExists(timezoneRoomName)) {
+    emitTimezoneRoomJoinResult("Room not found!", false);
+    return;
     }
-  });
+
+  const currentRoomName = getTimezoneRoomBySocket(socket);
+  if (timezoneRoomName === currentRoomName) {
+    emitTimezoneRoomJoinResult("You are already in this room!", false);
+    return;
 }
 
-//needs to send owner timezone when entering
-function onTimezoneRoomJoin(socket) {
-  socket.on("timezoneRoom:join", async (timezoneRoomName) => {
-    if (checkIfTimezoneRoomExists(timezoneRoomName)) {
-      const previousRoom = Array.from(socket.rooms)[1];
-      socket.leave(previousRoom);
+  socket.leave(currentRoomName);
       socket.join(timezoneRoomName);
       const socketsIds = Array.from(await io.in(timezoneRoomName).allSockets());
-      socket.emit(
-        "timezoneRoom:join:result",
+  emitTimezoneRoomJoinResult(
         "Joined successfully!",
         true,
         timezoneRoomName,
         socketsIds,
         roomOwner[timezoneRoomName]
       );
-    } else {
-      socket.emit("timezoneRoom:join:result", "Room not found!", false);
-    }
-  });
 }
 
 const io = new Server({
@@ -213,19 +209,39 @@ const io = new Server({
 io.on("connection", (socket) => {
   onConnection(socket);
   onTimezoneChanged(socket);
-  onTimezoneRoomCreate(socket);
-  onTimezoneRoomJoin(socket);
+  socket.on("timezoneRoom:create", (timezoneRoomName) => {
+    socket.on("disconnect", () => {
+      console.log(`> User ${socket.id} disconnected!`);
+    });
+
+    createTimezoneRoom(timezoneRoomName, socket);
+  });
+
+  //needs to send owner timezone when entering
+  socket.on("timezoneRoom:join", async (timezoneRoomName) => {
+    await handleTimezoneRoomJoin(socket, timezoneRoomName);
+    requestRefreshDateOfRoom(timezoneRoomName);
+  });
+
+  socket.on("dateForUpdate", (selectedTimezone, ownerDatetime) => {
+    if (
+      isSocketInATimezoneRoom(socket) &&
+      isSocketOwnerOfTimezoneRoom(socket)
+    ) {
+      updateDateOfARoom(socket, selectedTimezone, ownerDatetime);
+    }
+  });
 });
 
 io.of("/").adapter.on("delete-room", (room) => {
-  if (room in roomOwner) {
+  if (timezoneRoomExists(room)) {
     delete roomOwner[room];
     console.log(`Room ${room} deleted!`);
   }
 });
 
 io.of("/").adapter.on("leave-room", async (room, id) => {
-  if (room in roomOwner) {
+  if (timezoneRoomExists(room)) {
     const allSockets = await io.in(room).allSockets();
     const socketsIds = Array.from(allSockets);
     if (socketsIds.length > 0) {
@@ -234,6 +250,7 @@ io.of("/").adapter.on("leave-room", async (room, id) => {
       console.log(`> User ${id} left room ${room}`);
       console.log(`> Changed owner of ${room} from ${id} to ${newOwner}`);
       io.to(room).emit("timezoneRoom:userLeft", room, socketsIds, newOwner);
+      requestRefreshDateOfRoom(room);
     }
   }
 });
